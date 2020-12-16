ff59bb5bbbecc535608c96e95a837586d46a01dc
Lucas Pereira Vasconcelos <garrafote@gmail.com>
2020-12-16 10:35:18 +0000

    Better premake file

--- premake5.lua
+++ premake5.lua
@@ -1,6 +1,8 @@
 project "ImGui"
 	kind "StaticLib"
 	language "C++"
+	cppdialect "C++17"
+	staticruntime "on"
 
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
 	objdir ("intermediate/" .. outputdir .. "/%{prj.name}")
@@ -9,26 +11,23 @@ project "ImGui"
 	{
 		"imconfig.h",
 		"imgui.h",
+		"imgui.cpp",
+		"imgui_draw.cpp",
 		"imgui_internal.h",
+		"imgui_tables.cpp",
+		"imgui_widgets.cpp",
 		"imstb_rectpack.h",
 		"imstb_textedit.h",
 		"imstb_truetype.h",
-		"imgui.cpp",
-		"imgui_demo.cpp",
-		"imgui_draw.cpp",
-		"imgui_tables.cpp",
-		"imgui_widgets.cpp"
+		"imgui_demo.cpp"
 	}
 
 	filter "system:windows"
 		systemversion "latest"
-		cppdialect "C++17"
-		staticruntime "On"
 
 	filter "system:linux"
 		pic "On"
 		systemversion "latest"
-		cppdialect "C++17"
 		staticruntime "On"
 
 	filter "configurations:Debug"
@@ -38,3 +37,4 @@ project "ImGui"
 	filter "configurations:Release"
 		runtime "Release"
 		optimize "on"
+
